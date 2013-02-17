fd0d9ef9135d2d47e80c19df60254dc23c6f590d
Carlo Zottmann <carlo@municode.de>
2013-02-17 12:18:36 +0100

    [CHG] Renamed file, added tag names to output.

--- get_most_common_selectors.rb
+++ get_most_common_selectors.rb
@@ -4,28 +4,41 @@ require "nokogiri"
 require "trollop"
 
 opts = Trollop::options do
-  opt :folder, "lolder containing unpacked EPUB", :type => :string
+  opt :folder, "folder containing unpacked EPUB", :type => :string
   opt :verbose, "list number of occurrences with class name", :type => :boolean
 end
 
 Trollop::die "--folder is mandatory" unless opts[:folder]
 
-# Set up tally list
+folder = File.expand_path(opts[:folder])
+
+# Set up tally lists
 css_classes_occurence = {}
 css_classes_weighed = {}
 
 # Iterate over all HTML files in the folder
-Dir.glob("#{ opts[:folder] }/**/*.html").each do |path|
-  file = File.open("Blue Mars/Blue_Mars_split_000.html", "r") rescue nil
+Dir.glob("#{folder}/**/*.html").each do |path|
+  file = File.open(path, "r") rescue nil
   next unless file
 
   doc = Nokogiri::HTML(file)
   file.close
 
-  doc.css('body *').each do |node|
+  doc.css("body *").each do |node|
     css_class = node.attr(:class)
-    next if css_class.empty?
-    css_class = css_class.to_sym
+    next if !css_class || css_class.empty?
+
+    current_node = node
+    selector_tree = [ "#{node.description.name}.#{css_class}" ]
+
+    while parent_node = current_node.parent
+      break if parent_node == doc.root
+      selector_tree << "#{parent_node.description.name}.#{ parent_node.attr(:class) }"
+      current_node = parent_node
+    end
+
+    css_class = selector_tree.compact.reverse.join(" ")
+
     css_classes_occurence[css_class] = css_classes_occurence[css_class] || 0
     css_classes_occurence[css_class] += 1
   end
